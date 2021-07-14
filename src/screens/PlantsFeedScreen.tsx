efb87ca3a8d460f68b76d75c4a2fc2a88a0120f5
Marie Leuliette <marie.leuliette@gmail.com>
2021-07-14 16:55:59 +0200

    return datas on plantsfeedscreen not working

--- src/screens/PlantsFeedScreen.tsx
+++ src/screens/PlantsFeedScreen.tsx
@@ -1,22 +1,16 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Text, SafeAreaView } from "react-native";
 import { usePlants } from "../hooks/usePlants";
-
-interface plantProps {
-  name: string;
-  description: string;
-}
 
 const PlantsFeedScreen = () => {
   const { data } = usePlants();
 
   return (
-    <View>
-      <Text></Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-    </View>
+    <SafeAreaView>
+      {data.results.map((plant) => {
+        return <Text>{plant.name}</Text>;
+      })}
+    </SafeAreaView>
   );
 };
 
