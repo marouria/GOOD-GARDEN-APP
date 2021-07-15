30a6d4636720c02cdc449e8dd89b39b4bbc69f61
Marie Leuliette <marie.leuliette@gmail.com>
2021-07-15 18:10:30 +0200

    button view

--- src/screens/PlantsFeedScreen.tsx
+++ src/screens/PlantsFeedScreen.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { View, Text, SafeAreaView, ScrollView, Button } from "react-native";
-import { Card } from "react-native-paper";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  ImageBackground,
+} from "react-native";
+import { Avatar, Card, Title, Button } from "react-native-paper";
 import AppLayout from "../components/AppLayout";
 
 import { usePlants } from "../hooks/usePlants";
+
+const LeftContent = (props) => <Avatar.Icon {...props} icon="camera" />;
 
 const PlantsFeedScreen = () => {
   const { isLoading, isError, data } = usePlants();
@@ -16,18 +25,22 @@ const PlantsFeedScreen = () => {
     return <Text>Error...</Text>;
   }
 
+  function handlePress() {}
+
   return (
     <AppLayout>
       <SafeAreaView>
         {data.results.map((plant) => {
           return (
-            <Card>
-              <Card.Title
-                title={plant.name}
-                subtitle={plant.season}
-              ></Card.Title>
+            <Card style={styles.card}>
               <Card.Cover source={{ uri: plant.img_url }} />
-              <Card.Actions>{/* <Button>Ok</Button> */}</Card.Actions>
+              <Button
+                style={styles.button}
+                icon="camera"
+                mode="contained"
+                onPress={handlePress}
+              />
+              <Card.Title title={plant.name} subtitle="" left={LeftContent} />
             </Card>
           );
         })}
@@ -35,5 +48,17 @@ const PlantsFeedScreen = () => {
     </AppLayout>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    position: "absolute",
+    top: 10,
+  },
+  title: {
+    textAlign: "right",
+    // position: "absolute",
+    // top: 0,
+  },
+});
 
 export default PlantsFeedScreen;
