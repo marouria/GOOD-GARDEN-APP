147cb123b88869122c3eeab52a9052e33a654fc4
Marie Leuliette <marie.leuliette@gmail.com>
2021-07-16 10:57:21 +0200

    exporting api plant props

---
+++ api/types.d.ts
@@ -0,0 +1,12 @@
+export interface PlantProps {
+  id?: integer;
+  name?: string;
+  sunshine_rate?: integer;
+  watering?: integer;
+  img_url?: string;
+  season?: string;
+  user_level?: string;
+  price_cents?: integer;
+  description?: string;
+  currency_iso?: string;
+}
