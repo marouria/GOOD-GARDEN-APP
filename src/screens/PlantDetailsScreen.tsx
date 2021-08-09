a41f504321eb428ceba45aca8bac6c75955afa0e
Marie Leuliette <marie.leuliette@gmail.com>
2021-08-09 17:06:56 +0200

    plant details screen functionnal

--- src/screens/PlantDetailsScreen.tsx
+++ src/screens/PlantDetailsScreen.tsx
@@ -1,17 +1,81 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet, Button } from "react-native";
 import AppLayout from "../components/AppLayout";
+import { Avatar, DataTable } from "react-native-paper";
 
-interface Props {}
+interface Props {
+  route: any;
+}
 
 const PlantDetailsScreen = (props: Props) => {
+  const {
+    name,
+    description,
+    sunshine_rate,
+    watering,
+    img_url,
+    season,
+    user_level,
+    price_cents,
+    currency_iso,
+  } = props.route.params.plantId;
+
   return (
     <AppLayout>
       <View>
-        <Text>This is the details of a plant</Text>
+        <Text style={{ fontSize: 32, padding: 10 }}>{name}</Text>
+        <Image
+          source={{ uri: img_url }}
+          style={{ width: "100%", height: 300 }}
+        />
+        <DataTable>
+          <DataTable.Row style={styles.rowContainer}>
+            <DataTable.Cell style={styles.cellContainer}>
+              <Avatar.Icon
+                icon="white-balance-sunny"
+                color="orange"
+                style={{ backgroundColor: "none" }}
+              />
+            </DataTable.Cell>
+            <DataTable.Cell style={styles.cellContainer}>
+              <Avatar.Icon
+                icon="water"
+                color="blue"
+                style={{ backgroundColor: "none" }}
+              />
+            </DataTable.Cell>
+            <DataTable.Cell style={styles.cellContainer}>
+              <Avatar.Icon
+                icon="seed-outline"
+                color="green"
+                style={{ backgroundColor: "none" }}
+              />
+            </DataTable.Cell>
+          </DataTable.Row>
+          <DataTable.Row style={styles.rowContainer}>
+            <DataTable.Cell style={styles.cellContainer}>
+              {sunshine_rate}
+            </DataTable.Cell>
+            <DataTable.Cell style={styles.cellContainer}>
+              {watering}
+            </DataTable.Cell>
+            <DataTable.Cell style={styles.cellContainer}>
+              {season}
+            </DataTable.Cell>
+          </DataTable.Row>
+        </DataTable>
+        <Text>{description}</Text>
       </View>
     </AppLayout>
   );
 };
-
+const styles = StyleSheet.create({
+  cellContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  rowContainer: {
+    height: 50,
+  },
+});
 export default PlantDetailsScreen;
---
+++ src/theme/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  font: {
+    paragraph: "montserrat",
+  },
+  fontSize: {
+    paragraph: 8,
+    h1: 16,
+    h2: 24,
+  },
+};
