e12f6ecbfb8abd087afa8624223034001c7f90d2
Marie Leuliette <marie.leuliette@gmail.com>
2021-08-18 16:34:00 +0200

    questions & answers profile done

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -12,11 +12,17 @@ interface FaqItemsProps {
 const allQuestions = [
   {
     title: "Comment ça marche ?",
-    answer: "Ça marche comme ça",
+    answer:
+      "1. Ajoutez des plantes à votre jardin 2. Prenez connaissance des conseils d'entretien de GoodGarden 3. Créez de nouvelles tâches pour en prendre soin tout au long de l'année",
   },
   {
-    title: "Comment ça va ?",
-    answer: "Ça va comme ça",
+    title: "D'où viennent les plantes ?",
+    answer:
+      "Toutes les plantes sont soigneusement selectionnées par notre équipe auprès de producteurs locaux et s'assurent d'une production non intensive, respectueuse de l'environnement et durable.",
+  },
+  {
+    title: "Quels sont les niveaux de jardinnage ?",
+    answer: "",
   },
 ];
 
