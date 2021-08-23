81d362025cb1bc39fe4ca85d3b3b0cb9bbfe8a2b
Marie Leuliette <marie.leuliette@gmail.com>
2021-08-23 13:12:53 +0200

    adding date input to task form

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { View } from "react-native";
 import { Button, Headline, TextInput } from "react-native-paper";
-import AppLayout from "./AppLayout";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 type FormValues = {
   title: string;
@@ -10,26 +10,29 @@ type FormValues = {
   time: number;
   plant: string;
 };
+
 const TaskForm = () => {
   const {
-    register,
     control,
     handleSubmit,
-    watch,
+    setValue,
     formState: { errors },
   } = useForm<FormValues>();
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    alert("Task added to calendar");
+
+    console.log(data);
+  };
 
   return (
     <View>
       <Controller
         control={control}
-        render={({ field: { onChange, onBlur, value } }) => (
+        render={({ field: { onChange, value } }) => (
           <TextInput
             label="Add a title"
             mode="outlined"
-            onBlur={onBlur}
             onChangeText={onChange}
             value={value}
             style={{ padding: 10 }}
@@ -40,6 +43,23 @@ const TaskForm = () => {
         defaultValue="Water my plant"
       />
       {errors.title && <Headline>Title is required.</Headline>}
+
+      <Controller
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={value}
+            is24Hour={true}
+            display="spinner"
+          />
+        )}
+        name="date"
+        rules={{ required: true }}
+        defaultValue={new Date()}
+      />
+      {errors.title && <Headline>Title is required.</Headline>}
+
       <Button onPress={handleSubmit(onSubmit)}>Submit</Button>
     </View>
   );
--- src/screens/NewTaskScreen.tsx
+++ src/screens/NewTaskScreen.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { Headline } from "react-native-paper";
 import TaskForm from "../components/TaskForm";
 
 interface Props {}
 
 const NewTaskScreen = (props: Props) => {
   return (
-    <View>
-      <Text>Hellooooooo</Text>
-      <Text>Hellooooooo</Text>
-      <Text>Hellooooooo</Text>
+    <View style={{ margin: 20 }}>
+      <Headline>Create a new task</Headline>
       <TaskForm />
     </View>
   );
